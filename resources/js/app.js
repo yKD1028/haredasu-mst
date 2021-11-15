a881ba15bc147a8ea4acb89b00dddb87c2b40996
“Naryy” <narikawanariyuki@gmail.com>
2021-11-15 17:25:35 +0900

    laravel上でVueを表示できるように設定(SPA)

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,9 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+/*window.Vue = require('vue');*/
+
+import Vue from 'vue';
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,6 +22,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('header-comp', require('./components/Header.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
