225594f15cac1eb6c63f86ee61add6d43b5fd669
so_ananta <crusader0710@gmail.com>
2021-12-01 17:40:20 +0900

    usersにdeleted_atの追加

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,6 +27,7 @@ class CreateUsersTable extends Migration
       $table->string('address')->nullable();
       $table->rememberToken();
       $table->timestamps();
+      $table->softDeletes();
     });
   }
 
