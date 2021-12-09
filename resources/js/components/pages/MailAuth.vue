ff4a9852d37f82f1227c64889e3892582281db63
yokomizo <sky.mutsumi@gmail.com>
2021-12-09 10:33:22 +0900

    change:MailAuth.vue

--- resources/js/components/pages/MailAuth.vue
+++ resources/js/components/pages/MailAuth.vue
@@ -1,14 +1,12 @@
 <template>
     <div class="home_wrap">
-      <p>はじめまして！</p>
-      <p>メールアドレスで登録して晴れ予約を開始しましょう！</p>
-        <div class="home_wrap">
-            <Header></Header>
-            <FormEmail placeholder="haredas@gmail.com" name="sample-input" type="text" form_title="メールアドレス"></FormEmail>
-            <OrangeButton button_name="確認メールを送信"></OrangeButton>
-        </div>
+        <p>はじめまして！</p>
+        <p>メールアドレスで登録して<br>晴れ予約を開始しましょう！</p>
+        <Header></Header>
+        <FormEmail placeholder="haredas@gmail.com" name="email" form_title="メールアドレス"></FormEmail>
+        <OrangeButton button_name="確認メールを送信"></OrangeButton>
         <p>または</p>
-        <p><a href="#">ログイン</a></p>
+        <p><a href="/login">ログイン</a></p>
     </div>
 </template>
 
