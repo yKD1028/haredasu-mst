c2645b74deeebf81dc70243db6e9b18f26535488
Naryy <narikawanariyuki@gmail.com>
2022-01-07 03:01:48 +0900

    add:favicon fix:mainvisual

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <title>Example</title>
+    <!--<link rel="icon" href="images/favicon.svg" type="image/svg+xml">-->
+    <link rel="shortcut icon" href="{{ asset('/favicon.svg') }}" type="image/svg+xml">
+    <title>ハレダス</title>
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
