5b84c25f04a16d377b5fb5350c1338a3f9368f04
Naoyuki <naoyukisato20@gmail.com>
2022-01-08 08:25:51 +0900

    Googleマップ置いたもの

--- resources/js/components/components/mypage_map.vue
+++ resources/js/components/components/mypage_map.vue
@@ -1,10 +1,47 @@
 <template>
     <div class="mypage_map">
         <!-- mapのコード(仮) -->
-        <img :src="'/images/test/map.png'" alt="仮の画像" width="100%">
+        <!-- <img :src="'/images/test/map.png'" alt="仮の画像" width="100%"> -->
+        <div class="map" ref="googleMap" />
     </div>
 </template>
 
 <script>
+import GoogleMapsApiLoader from 'google-maps-api-loader';
 
+export default {
+  name: 'Map',
+  data() {
+    return {
+      google: null,
+      mapConfig: {
+        center: {
+          lat: 35.68944,
+          lng: 139.69167
+        },
+        zoom: 15
+      }
+    }
+  },
+  //Reserve 256行目くらい
+  async mounted() {
+    this.google = await GoogleMapsApiLoader({
+      apiKey: 'AIzaSyCbr524Eht2tpaHaFLvBShbHBy1m1uqBy4'
+    });
+    this.initializeMap();
+  },
+  //Reserve 266行目くらい
+  methods: {
+    initializeMap() {
+      new this.google.maps.Map(this.$refs.googleMap, this.mapConfig);
+    }
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+.map {
+  width: 79vw;
+  height: 50vh;
+}
+</style>
