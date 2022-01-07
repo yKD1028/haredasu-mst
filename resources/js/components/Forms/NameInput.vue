bcdbec03633b362d4f35e95ddeb3d8616f4ebbd5
yokomizo <sky.mutsumi@gmail.com>
2022-01-08 00:22:19 +0900

    change:fix errorEvent

--- resources/js/components/Forms/NameInput.vue
+++ resources/js/components/Forms/NameInput.vue
@@ -25,65 +25,61 @@ export default {
       name: "",
       formChange: "",
       errors: {},
+      err: Boolean,
     };
   },
   watch: {
     name(name) {
-      //漢字カタカナひらがな全角半角英字大文字小文字
-      //\u4E00-\u9FA0：漢字(一-龠)、u3005-3007：々〆〇(漢数字)、3041-3094：ひらがな、\u309D:ゝ\u309E:ゞ
-      //30A1-30F6：カタカナ、30FB-30FE：・ーヽヾ
-      var pattern =
-        /^[A-Za-zＡ-Ｚａ-ｚ\u4E00-\u9FA0\u3005-\u3007\u3041-\u3094\u309D\u309E\u30A1-\u30F6\u30FB-\u30FE]+$/;
-      var err = true;
-      if (!name) {
-        this.$set(this.errors, "name", "氏名を入力してください。");
-      } else if (!name.match(pattern)) {
-        this.$set(
-          this.errors,
-          "name",
-          "氏名に使用できない文字が含まれています。"
-        );
-      } else if (name.length > 50) {
-        this.$set(this.errors, "name", "氏名が長すぎます。");
-      } else {
-        this.$delete(this.errors, "name");
-        err = false;
-      }
-
-      if (err) {
+      this.errCheck(name);
+      this.$emit("err", this.err);
+      this.$emit("input", name);
+      if (this.err) {
         this.colorChange("form_err");
       } else {
         this.colorChange("form_focus");
       }
-      this.$emit("err", err);
-      this.$emit("input", name);
     },
   },
   mounted() {
     this.name = this.value;
   },
   methods: {
+    errCheck(name) {
+      var pattern =
+        /^[A-Za-zＡ-Ｚａ-ｚ\u4E00-\u9FA0\u3005-\u3007\u3041-\u3094\u309D\u309E\u30A1-\u30F6\u30FB-\u30FE]+$/;
+      if (!name) {
+        this.$set(this.errors, "name", "氏名を入力してください。");
+        this.err = true;
+      } else if (!name.match(pattern)) {
+        this.$set(
+          this.errors,
+          "name",
+          "氏名に使用できない文字が含まれています。"
+        );
+        this.err = true;
+      } else if (name.length > 50) {
+        this.$set(this.errors, "name", "氏名が長すぎます。");
+        this.err = true;
+      } else {
+        this.$delete(this.errors, "name");
+        this.err = false;
+      }
+    },
     colorChange: function (color) {
       this.formChange = color;
     },
     onFocus: function () {
-      if (this.errors.name === undefined) {
-        this.colorChange("form_focus");
-      }
-      this.$emit("err", false);
+      this.$delete(this.errors, "name");
+      this.colorChange("form_focus");
     },
     onBlur: function () {
-      var err = true;
-      if (this.errors.name) {
+      this.errCheck(this.name);
+      this.$emit("err", this.err);
+      if (this.err) {
         this.colorChange("form_err");
-      } else if (!this.name) {
-        this.$set(this.errors, "name", "氏名を入力してください。");
-        this.colorChange("form_err");
-      } else if (this.errors.name === undefined) {
+      } else {
         this.colorChange("");
-        err = false;
       }
-      this.$emit("err", err);
     },
   },
 };
