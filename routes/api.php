a30968245b4d9601dd857876ac0e4a414a3df889
so_ananta <crusader0710@gmail.com>
2022-01-08 16:09:51 +0900

    corsの変更

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//   return $request->user();
+// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
   return $request->user();
 });
 //auth
