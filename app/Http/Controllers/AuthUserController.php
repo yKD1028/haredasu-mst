c0595170129d3a59f872ae0e5cf4d7afd7d56256
so_ananta <crusader0710@gmail.com>
2022-01-08 16:13:32 +0900

    AuthUserControllerの作成及びログインログアウト処理,UserControllerの不要な処理の削除

---
+++ app/Http/Controllers/AuthUserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthUserController extends Controller
+{
+  //
+  /**
+   * ログイン
+   */
+  public function login(Request $request)
+  {
+    $result = false;
+    $message = '';
+    $user = [];
+    $credentials = $request->only('email', 'password');
+    if (Auth::attempt($credentials)) {
+      $request->session()->regenerate();
+      $result = true;
+    } else {
+      $message = 'EmailまたはPasswordが間違っています。';
+    }
+    return response()->json(['result' => $result, 'message' => $message]);
+  }
+  /**
+   * ログアウト
+   */
+  public function logout(Request $request)
+  {
+    $result = true;
+    $message = 'ログアウトしました。';
+    Auth::logout();
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+    return response()->json(['result' => $result, 'message' => $message]);
+  }
+}
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,29 +20,6 @@ class UsersController extends Controller
   //   $this->middleware('verified');
   // }
 
-  public function user_info()
-  {
-    $user = Auth::User();
-    $cardList = [];
-    $userData = auth()->user();
-    // 既にpayjpに登録済みの場合
-    if (!empty($userData->payjp_customer_id)) {
-      // カード一覧を取得
-      \Payjp\Payjp::setApiKey(config('payjp.secret_key'));
-      $cardDatas = \Payjp\Customer::retrieve($userData->payjp_customer_id)->cards->data;
-      foreach ($cardDatas as $cardData) {
-        $cardList[] = [
-          'cardNumber' =>  "**** **** **** {$cardData->last4}",
-          'brand' =>  $cardData->brand,
-          'exp_year' =>  $cardData->exp_year,
-          'exp_month' =>  $cardData->exp_month,
-          'name' =>  $cardData->name,
-          'id' => $cardData->id,
-        ];
-      }
-    }
-    return view('test.user_info', ['user' => $user], compact('cardList'));
-  }
 
   public function regist_user_info(Request $post)
   {
